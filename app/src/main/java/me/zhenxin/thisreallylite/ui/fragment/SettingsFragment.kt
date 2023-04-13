a89677eac467c8b2d97fcb69cd68e098f16d171f
真心 <qgzhenxin@qq.com>
2023-04-13 18:31:35 +0800

    fix: 替换过时的API

--- app/src/main/java/me/zhenxin/thisreallylite/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/me/zhenxin/thisreallylite/ui/fragment/SettingsFragment.kt
@@ -3,6 +3,7 @@ package me.zhenxin.thisreallylite.ui.fragment
 import android.os.Bundle
 import androidx.preference.Preference
 import com.highcapable.yukihookapi.YukiHookAPI
+import com.highcapable.yukihookapi.YukiHookAPI.Status.Executor
 import com.highcapable.yukihookapi.hook.xposed.prefs.ui.ModulePreferenceFragment
 import me.zhenxin.thisreallylite.BuildConfig
 import me.zhenxin.thisreallylite.R
@@ -21,7 +22,7 @@ class SettingsFragment : ModulePreferenceFragment() {
         val status = findPreference<Preference>("status")
         if (YukiHookAPI.Status.isModuleActive) {
             status?.summary =
-                "${getString(R.string.active_title)}(${YukiHookAPI.Status.executorName})"
+                "${getString(R.string.active_title)}(${Executor.name})"
         }
 
         val version = findPreference<Preference>("version")
